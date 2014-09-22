5da9275ddbaef758c2735a34ac3bb699ce7924db
pthieuklick <pthieu@klick.com>
2014-09-22 15:30:34 -0400

    checking out qhat jquery they use

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 $('body').find('#originalContent strong').html('I used to be an ASIC Design/Verification Engineer at Qualcomm -- did something like design part of the Snapdragon processor -- but that became really un-fun after a few years. Now I make web sites for pharmaceutical companies, and that\'s way cooler :v');
-$('.box:contains(\'Web Developer\')').find('span:contains(\'Web Developer\')').each(function(){$(this).text('Master Web Developer & Superstar')});
+$('.box:contains(\'Web Developer\')').find('span:contains(\'Web Developer\')').each(function() {
+	debugger;
+  // $(this).text('Master Web Developer & Superstar');
+});
