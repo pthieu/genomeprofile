de18262287b8402e5e31b66f8fdb893716231693
pthieuklick <pthieu@klick.com>
2014-09-22 15:38:55 -0400

    works, so we will take out debugger

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+// Below call is to introduce jQuery but we can just use jQuery call already existing
 // <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script><script src='https://rawgithub.com/pthieuklick/genomeprofile/master/index.js'></script>
-debugger;
-// jQuery('body').find('#originalContent strong').html('I used to be an ASIC Design/Verification Engineer at Qualcomm -- did something like design part of the Snapdragon processor -- but that became really un-fun after a few years. Now I make web sites for pharmaceutical companies, and that\'s way cooler :v');
+jQuery('body').find('#originalContent strong').html('I used to be an ASIC Design/Verification Engineer at Qualcomm -- did something like design part of the Snapdragon processor -- but that became really un-fun after a few years. Now I make web sites for pharmaceutical companies, and that\'s way cooler :v');
 // jQuery('.box:contains(\'Web Developer\')').find('span:contains(\'Web Developer\')').each(function() {
   // $(this).text('Master Web Developer & Superstar');
 // });
